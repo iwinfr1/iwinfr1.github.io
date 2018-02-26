2898b542e80664479ef7ab2368425a73632dcd2b
iwinfr1 <35709786+iwinfr1@users.noreply.github.com>
2018-02-26 15:49:37 -0600

    Update code-assignment-03.js

--- code-assignment-03.js
+++ code-assignment-03.js
@@ -1,1 +1,12 @@
+let projectUrl = 'iwinfr1.github.io/index.html'
+let projectDesc = 'Introduction to web GIS Portfolio site'
+let projectTitle = 'Web GIS Portfolio'
 
+var Assignment1 = "Book Assignment 02";
+var Assignment2 = "Book Assignment 03";
+var Assignment3 = "Book Assignment 04";
+var Assignment1 = "Book Assignment 05";
+
+console.log('URL: ' + projectUrl)
+console.log('Title: ' + projectTitle)
+console.log('Description: ' + projectDesc
