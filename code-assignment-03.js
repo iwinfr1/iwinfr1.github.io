7564fea44ca924977ce9f7e805cad47f216a9270
iwinfr1 <35709786+iwinfr1@users.noreply.github.com>
2018-02-26 15:53:28 -0600

    Update code-assignment-03.js

--- code-assignment-03.js
+++ code-assignment-03.js
@@ -1,4 +1,5 @@
-let projectUrl = 'iwinfr1.github.io/index.html'
+// assign values to a set of variables representing a project's metadatalet 
+projectUrl = 'iwinfr1.github.io/index.html'
 let projectDesc = 'Introduction to web GIS Portfolio site'
 let projectTitle = 'Web GIS Portfolio'
 
@@ -7,6 +8,7 @@ var Assignment2 = "Book Assignment 03";
 var Assignment3 = "Book Assignment 04";
 var Assignment1 = "Book Assignment 05";
 
+// display messages in the browser's developer console, combining the values with text
 console.log('URL: ' + projectUrl)
 console.log('Title: ' + projectTitle)
 console.log('Description: ' + projectDesc
