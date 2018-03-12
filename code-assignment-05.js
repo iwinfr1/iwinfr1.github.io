b82302fa80236d9effea679c10499b1dc9439748
iwinfr1 <35709786+iwinfr1@users.noreply.github.com>
2018-03-12 15:59:15 -0500

    Create code-assignment-05.js

---
+++ code-assignment-05.js
@@ -0,0 +1,37 @@
+let projectObject = {
+Quantity: 10,
+Url: 'iwinfr1.github.io/index.html',
+Desc: 'Introduction to web GIS Portfolio site',
+Title: 'Web GIS Portfolio',
+assignments: true,
+ 'assignments',
+ 'Title',
+ 'Desc'
+  ]
+}
+
+let projectArray = [
+ 10,
+ 'iwinfr1.github.io/index.html',
+ 'Introduction to web GIS Portfolio site',
+ 'Web GIS Portfolio',
+ true, 
+['assignments', 'Desc', 'Title']
+]
+
+
+let projectQuantity = 10
+let projectUrl = 'iwinfr1.github.io/index.html'
+let projectDesc = 'Introduction to web GIS Portfolio site'
+let projectTitle = 'Web GIS Portfolio'
+let projectassignments = true
+let projectlength = 120.3
+
+let Assignment1 = "Book Assignment 02";
+let Assignment2 = "Book Assignment 03";
+let Assignment3 = "Book Assignment 04";
+let Assignment1 = "Book Assignment 05";
+
+console.log('URL: ' + projectUrl)
+console.log('Title: ' + projectTitle)
+console.log('Desc: ' + projectDesc
