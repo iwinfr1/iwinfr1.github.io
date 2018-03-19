8154709a553db07237128ff88a5f664aa85b4683
iwinfr1 <35709786+iwinfr1@users.noreply.github.com>
2018-03-19 12:17:16 -0500

    Create code-assignment-06a.js

---
+++ code-assignment-06a.js
@@ -0,0 +1,63 @@
+let projectObject = {
+Quantity: 10,
+Url: 'iwinfr1.github.io/index.html',
+Desc: 'Introduction to web GIS Portfolio site',
+Title: 'Web GIS Portfolio',
+assignments: true,
+ 'assignments',
+ 'Title',
+ 'Desc'
+  ]
+}
+
+let projectArray = [
+ 10,
+ 'iwinfr1.github.io/index.html',
+ 'Introduction to web GIS Portfolio site',
+ 'Web GIS Portfolio',
+ true, 
+['assignments', 'Desc', 'Title']
+]
+
+
+let projectQuantity = 10
+let projectUrl = 'iwinfr1.github.io/index.html'
+let projectDesc = 'Introduction to web GIS Portfolio site'
+let projectTitle = 'Web GIS Portfolio'
+let projectassignments = true
+let projectlength = 120.3
+
+let Assignment1 = "Book Assignment 02";
+let Assignment2 = "Book Assignment 03";
+let Assignment3 = "Book Assignment 04";
+let Assignment1 = "Book Assignment 05";
+
+console.log('URL: ' + projectUrl)
+console.log('Title: ' + projectTitle)
+console.log('Desc: ' + projectDesc
+            
+
+
+let Assignment1 = {
+    title: "Book Assignment 02",
+    id: 1,
+    type: true
+}
+let Assignment2 = {
+    title: "Book Assignment 03";
+    id: 2
+    type: true
+}
+let assignments = [
+   assignment1,
+   assignment2
+]
+
+for (i=0; i<5; i++)
+ 
+console.log('title ' + i + 'projectDesc ' + Assignment[i].Desc)
+console.log('assignments ' + i + ' id: ' + Assignment[i].quantity
+       
+if (assignment[i].type === true {
+  console.log('title ' + i + 'projectDesc ' + Assignment[i].Desc)
+}
