b640a6bf3c02e2196213237a46f4b255c2613d4e
iwinfr1 <35709786+iwinfr1@users.noreply.github.com>
2018-04-16 13:54:30 -0500

    Update webmap-assignment-05.js

--- map/webmap-assignment-05.js
+++ map/webmap-assignment-05.js
@@ -2,10 +2,12 @@ var mymap = L.map('llmap').setView([30.450171, -91.123621], 12);
 
 var worldbasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}').addTo(mymap);
 var streetbasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(mymap);
+var topographic = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(mymap);
 
 var newBasemaps = {
   'World Basemap': worldbasemap,
-  'Street Basemap': streetbasemap
+  'Street Basemap': streetbasemap,
+  'Topographic': topopraphic
 }
 L.control.layers(newBasemaps).addTo(mymap)
 
@@ -17,7 +19,7 @@ var myStyle = {
  weight: '4',
  fillColor: 'none'
 }
-var myOptions = {
+var styoption = {
   style: myStyle,
   onEachFeature: myPopup
  }
