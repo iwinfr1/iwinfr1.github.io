a3b9365934e395d00ab0ac8e6ecce982ce1bf9ba
iwinfr1 <35709786+iwinfr1@users.noreply.github.com>
2018-05-04 11:44:02 -0500

    Create geoj.js

---
+++ Map/Fin/geoj.js
@@ -0,0 +1,299 @@
+var geojsonmap = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -122.8069,38.0691 ]
+    },
+    "properties": {
+    "Internship":"  Invasive Plant Team Biological Intern",
+    "Category":"  Forestry, Recreation, Restoration",
+    "Description":"   Allows the opportunity to learn how NPS and seashore association programs operate, as well as on-the-ground tangible restoration",
+    "Website":"  http://www.ptreyes.org",
+    "Location":"Point Reyes Station , California"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -122.7095,42.1946 ]
+    },
+    "properties": {
+    "Internship":"  Wilderness Conservation Corps Internship",
+    "Category":"  Environmental Education, Forestry, Recreation, Restoration",
+    "Description":"   Fire trail rehab and maintenance",
+    "Website":"  http://www.siskiyoumountainclub.org/wcc2018",
+    "Location":"ASHLAND , Oregon"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 28.034088,-26.195246 ]
+    },
+    "properties": {
+    "Internship":"  Environment & Wildlife Volunteer - Nature Conservation in South Africa",
+    "Category":"  Environmental Education, Restoration, Wildlife",
+    "Description":"  The Wildlife For All Trust is a British registered environmental charity with a pioneering approach to nature conservation",
+    "Website":"  http://wildlifeforall.org",
+    "Location":"Johannesburg, South Africa"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -122.4194,37.7749 ]
+    },
+    "properties": {
+    "Internship":"  Trail Construction and Maintenance Internship",
+    "Category":"  Recreation, Restoration",
+    "Description":"  Engage and sustain a vibrant community of stewards in the maintenance and construction of trails within the Golden Gate National Parks",
+    "Website":"  http://www.parksconservancy.org/get-involved/volunteer/internships/trail-construction-maintenance-internship.html",
+    "Location":"San Francisco , California"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -114.3637,43.6807 ]
+    },
+    "properties": {
+    "Internship":"  Conservation Education Resources Assistant",
+    "Category":"  Environmental Education, Forestry, Recreation, Restoration",
+    "Description":"   Complete research, develop and present natural and cultural walks, talks and campfire programs for diverse groups of visiting youth and adults, including wilderness education (Leave No Trace) in the outdoors",
+    "Website":"  http://www.idahocc.org",
+    "Location":"Ketchum , Idaho"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -115.183724,45.3624118 ]
+    },
+    "properties": {
+    "Internship":"  Wilderness Trails Forestry Technician",
+    "Category":"  Forestry, Recreation, Restoration",
+    "Description":"  Clear downfall and brush from multiple-use trails",
+    "Website":"  http://www.idaho-conservationcorps.org/ourprograms/ConservationInterns",
+    "Location":"Chamberlain Wilderness Station , Idaho"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -75.5398,39.7391 ]
+    },
+    "properties": {
+    "Internship":"  Summer Zoo Camp Educator Internship",
+    "Category":"  Environmental Education, Recreation, Wildlife",
+    "Description":"  Interns will work with adult and teen volunteers, live animals, and will help children learn about our natural world",
+    "Website":"  http://www.brandywinezoo.org",
+    "Location":"Wilmington , Delaware"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.4013,40.0581 ]
+    },
+    "properties": {
+    "Internship":"  Environmental Professional",
+    "Category":"  Botany, Ecology, Environmental Education, Restoration, Wildlife",
+    "Description":"   Conservation of natural resources, landscape management and horticulture through training alongside Arboretum staff",
+    "Website":"  https://dawesarb.org/",
+    "Location":"Newark , Ohio"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -70.8773,42.8126 ]
+    },
+    "properties": {
+    "Internship":"  Family & Youth Education Internship",
+    "Category":"  Ecology, Environmental Education, Fisheries, Marine Biology, Wetland, Wildlife",
+    "Description":"  Assisting with the implementation of school programs and field trips offered onsite at the Joppa Flats Education Center and offsite on the Parker River National Wildlife Refuge and Plum Island",
+    "Website":"  https://www.massaudubon.org",
+    "Location":"Newburyport , Massachusetts"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.4915,43.7127 ]
+    },
+    "properties": {
+    "Internship":"  Lakes Region Conservation Corps",
+    "Category":"  Ecology, Environmental Education, Land Trust, Recreation",
+    "Description":"  Receive training in land conservation methods, conservation and recreational land management, and education and outreach",
+    "Website":"  https://www.squamlakes.org/summer-lrcc#lrct",
+    "Location":"Center Harbor , New Hampshire"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -104.9903,39.7392 ]
+    },
+    "properties": {
+    "Internship":"  Environmental Nonprofit Graduate Intern Volunteer",
+    "Category":"  Ecology, Environmental Education, Non-Profit Development",
+    "Description":"   Improve Colorado cities using tree science; restore natural systems and urban green infrastructure; and educate children and adults to become environmental stewards",
+    "Website":"  http://www.i4es.org",
+    "Location":"Denver , Colorado"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -122.1375,37.3797 ]
+    },
+    "properties": {
+    "Internship":"  Development Internship",
+    "Category":"  Environmental Education, Non-Profit Development",
+    "Description":"   Impart a solid, working understanding of nonprofit administration and fundraising.  This internship provides the opportunity to live, work and learn at an established non-profit education organization and to participate in classes and other opportunities with other Hidden Villa staff",
+    "Website":"  http://www.hiddenvilla.org",
+    "Location":"Los Altos Hills , California"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -110.9747,32.2226 ]
+    },
+    "properties": {
+    "Internship":"  Corpsmember - Chiricahua Field Crew",
+    "Category":"  Forestry, Land Trust, Recreation, Restoration, Wetland, Wildlife",
+    "Description":"  Trail construction and maintenance, ecosystem restoration, habitat restoration, fire fuels reduction, fencing on public lands, invasive weed removal, herbicide applications, and many others",
+    "Website":"  https://azcorps.org/open-positions",
+    "Location":"Tucson , Arizona"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -119.8138,39.5296 ]
+    },
+    "properties": {
+    "Internship":"  Forestry Intern: Whitebark Pine Inventory and Monitoring",
+    "Category":"  Forestry",
+    "Description":"  Establish baseline conditions of forest health and structure",
+    "Website":"  http://www.thegreatbasininstitute.org",
+    "Location":"Reno , Nevada"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -22.9103,16.6001 ]
+    },
+    "properties": {
+    "Internship":"  Field Assistant with Sea Turtles in Sal Island, Cabo Verde 2018",
+    "Category":"  Marine Biology, Wildlife",
+    "Description":"   Provides direct beach protection, as well as grassroots education and engagement",
+    "Website":"  http://www.projectbiodiversity.org",
+    "Location":"Santa Maria , Africa"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -111.6513,35.1983 ]
+    },
+    "properties": {
+    "Internship":"  Corpsmember - Invasive Weeds Field Crew",
+    "Category":"  Botany, Ecology, Forestry, Recreation, Restoration",
+    "Description":"  Works directly with the Coconino National Forest to conduct field inventories on plant species",
+    "Website":"  https://azcorps.org/open-positions",
+    "Location":"Flagstaff , Arizona"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -106.7637,32.3199 ]
+    },
+    "properties": {
+    "Internship":"  Conservation Corps New Mexico- Field Crew Member",
+    "Category":"  Ecology, Forestry, Land Trust, Recreation, Restoration, Wetland",
+    "Description":"   Trail construction and maintenance, ecosystem restoration, habitat restoration, fire fuels reduction, fencing on public lands, invasive weed removal, herbicide applications, and many others",
+    "Website":"  https://corpsnm.org/open-positions",
+    "Location":"Las Cruces , New Mexico"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 33.7741,-13.9626 ]
+    },
+    "properties": {
+    "Internship":"  Bat Research Assistant",
+    "Category":"  Ecology, Wildlife",
+    "Description":"  Conduct research on bats and insects out of our field research center based in Vwaza Marsh Wildlife Reserve in  Northern Malawi",
+    "Website":"  http://www.africanbatconservation.org",
+    "Location":"Lilongwe , Africa"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -119.8138,39.5296 ]
+    },
+    "properties": {
+    "Internship":"  Rare Plant Survey Intern",
+    "Category":"  Botany, Ecology",
+    "Description":"  Perform botanical surveys within the northeastern region of the Lake Tahoe Basin",
+    "Website":"  http://www.thegreatbasininstitute.org",
+    "Location":"Reno , Nevada"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -118.7617,34.1533 ]
+    },
+    "properties": {
+    "Internship":"  Intern, Nesting Raptor Study",
+    "Category":"  Ecology, Recreation, Wildlife",
+    "Description":"  Revisit known nest sites of multiple raptor species dating back to the early 1970s, and will investigate site fidelity and habitat preferences",
+    "Website":"  http://www.cooperecological.com",
+    "Location":"Agoura Hills , California"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -107.8743,48.3597 ]
+    },
+    "properties": {
+    "Internship":"  Paid Grassland Ecology Internship",
+    "Category":"  Botany, Ecology, Wildlife",
+    "Description":"   Support staff activities associated with ecology projects and play a significant role in the advancement of conservation science ",
+    "Website":"  https://www.conservationjobboard.com/job-listing-paid-grassland-ecology-internship-malta-montana/729851689",
+    "Location":"Malta , Montana"
+    }
+  }
+]
+}
